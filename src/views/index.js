d0cadbfb9f3d74f7a7cc3446eec92f38f4716237
TopSM <ostavo.p@gmail.com>
2020-07-08 19:36:53 -0400

    appfairy: Migrate

--- src/views/index.js
+++ src/views/index.js
@@ -1,14 +1,14 @@
 /* eslint-disable */
 
-export { default as UnauthorizedView } from './UnauthorizedView'
-export { default as NotFoundView } from './NotFoundView'
-export { default as CalendarView } from './CalendarView'
-export { default as ImageLicenseInfoView } from './ImageLicenseInfoView'
-export { default as IndexView } from './IndexView'
-export { default as LaesaProjectsView } from './LaesaProjectsView'
-export { default as LoginView } from './LoginView'
-export { default as MakeAccountView } from './MakeAccountView'
-export { default as MembersView } from './MembersView'
-export { default as PcedView } from './PcedView'
+export { default as 40172929PMView } from './40172929PMView'
+export { default as 40472929PMView } from './40472929PMView'
+export { default as Calendar72929PMView } from './Calendar72929PMView'
+export { default as ImageLicenseInfo72929PMView } from './ImageLicenseInfo72929PMView'
+export { default as Index72929PMView } from './Index72929PMView'
+export { default as LaesaProjects72929PMView } from './LaesaProjects72929PMView'
+export { default as Login72929PMView } from './Login72929PMView'
+export { default as MakeAccount72929PMView } from './MakeAccount72929PMView'
+export { default as Members72929PMView } from './Members72929PMView'
+export { default as Pced72929PMView } from './Pced72929PMView'
 
 /* eslint-enable */
