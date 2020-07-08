757c1b1a5e9f314a6b7e3def5e2d50b29eafe770
TopSM <ostavo.p@gmail.com>
2020-07-08 19:35:35 -0400

    fixing errors

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+import "./scripts";
+import "./styles";
+import IndexViews from "./views/IndexView";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <IndexViews />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
