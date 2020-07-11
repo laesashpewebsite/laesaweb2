07c2724d4615db8779957c197a2741d231b33408
TopSM <ostavo.p@gmail.com>
2020-07-10 22:07:31 -0400

    new sockets

--- src/Controllers/LoginControllers.jsx
+++ src/Controllers/LoginControllers.jsx
@@ -1,8 +1,4 @@
 import React, { Component } from "react";
 import LoginView from "../views/LoginView";
 
-export default props => (
-  <LoginView>
-    <Login {...props} />
-  </LoginView>
-);
+export default props => <LoginView>{/* <login {...props} /> */}</LoginView>;
